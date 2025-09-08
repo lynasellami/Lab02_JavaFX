a51528252961895982f06ff167eeab6f494fc25e
Lyna <2488446@edu.vaniercollege.qc.ca>
2025-09-08 15:25:18 -0800

    feat: add top/bottom labels (Random Game, Waiting…)

--- src/Lab02_LynaSelami.java
+++ src/Lab02_LynaSelami.java
@@ -1,5 +1,7 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -7,7 +9,18 @@ public class Lab02_LynaSelami extends Application {
 
     @Override
     public void start(Stage stage) {
-        BorderPane root = new BorderPane(); // placeholder root
+        BorderPane root = new BorderPane();
+        root.setPadding(new Insets(10));
+
+        Label lblTop = new Label("Random Game");
+        Label lblBottom = new Label("Waiting…");
+
+        BorderPane.setMargin(lblTop, new Insets(5));
+        BorderPane.setMargin(lblBottom, new Insets(5));
+
+        root.setTop(lblTop);
+        root.setBottom(lblBottom);
+
         Scene scene = new Scene(root, 250, 300);
         stage.setTitle("Java Games");
         stage.setScene(scene);
@@ -18,3 +31,4 @@ public class Lab02_LynaSelami extends Application {
         launch(args);
     }
 }
+
