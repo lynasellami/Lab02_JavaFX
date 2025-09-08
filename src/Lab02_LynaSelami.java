90ade005c1a430b71bd3b0b70a521ab640177814
Lyna <2488446@edu.vaniercollege.qc.ca>
2025-09-08 15:28:45 -0800

    feat(ui): center VBox + placeholder image label

--- src/Lab02_LynaSelami.java
+++ src/Lab02_LynaSelami.java
@@ -1,8 +1,10 @@
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Lab02_LynaSelami extends Application {
@@ -14,12 +16,16 @@ public class Lab02_LynaSelami extends Application {
 
         Label lblTop = new Label("Random Game");
         Label lblBottom = new Label("Waiting…");
-
         BorderPane.setMargin(lblTop, new Insets(5));
         BorderPane.setMargin(lblBottom, new Insets(5));
-
         root.setTop(lblTop);
         root.setBottom(lblBottom);
+
+        Label lblImage = new Label("Image will appear here"); // placeholder text for now
+        VBox middle = new VBox(lblImage);
+        middle.setAlignment(Pos.CENTER);
+        middle.setPadding(new Insets(10));
+        root.setCenter(middle);
 
         Scene scene = new Scene(root, 250, 300);
         stage.setTitle("Java Games");
